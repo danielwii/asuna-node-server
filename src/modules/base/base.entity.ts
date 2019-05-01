1d2be07f7d5eb475b0ffb2604e44359e66410f7d
Daniel Wei <danielwii0326@gmail.com>
2019-05-01 20:59:15 +0800

    feat: add uuid base entity support

--- src/modules/base/base.entity.ts
+++ src/modules/base/base.entity.ts
@@ -9,7 +9,7 @@ import {
 import { MetaInfo } from '../decorators';
 
 export abstract class AbstractBaseEntity extends BaseEntity {
-  @PrimaryGeneratedColumn() id: number;
+  @PrimaryGeneratedColumn() id!: number;
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
@@ -23,6 +23,30 @@ export abstract class AbstractBaseEntity extends BaseEntity {
 }
 
 export abstract class AbstractNameEntity extends AbstractBaseEntity {
+  @MetaInfo({ name: '名称' })
+  @Column({ nullable: false, length: 100, unique: true, name: 'name' })
+  name: string;
+
+  @MetaInfo({ name: '描述' })
+  @Column('text', { nullable: true, name: 'description' })
+  description: string;
+}
+
+export abstract class AbstractUUIDBaseEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid') uuid!: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @MetaInfo({ accessible: 'hidden' })
+  @Column({ nullable: true, length: 100, name: 'updated_by' })
+  updatedBy: string;
+}
+
+export abstract class AbstractUUIDNameEntity extends AbstractUUIDBaseEntity {
   @MetaInfo({ name: '名称' })
   @Column({ nullable: false, length: 100, unique: true, name: 'name' })
   name: string;
