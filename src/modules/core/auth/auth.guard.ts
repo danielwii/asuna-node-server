cb6d6175e8b5ae4735a78342ec328ece52775bf3
Daniel Wei <danielwii0326@gmail.com>
2019-05-11 18:44:15 +0800

    feat: add opts to jwt auth guard

--- src/modules/core/auth/auth.guard.ts
+++ src/modules/core/auth/auth.guard.ts
@@ -1,14 +1,20 @@
-import { ExecutionContext, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
-import * as passport from 'passport';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 const logger = new Logger('JwtAuthGuard');
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly opts: { anonymousSupport: boolean } = { anonymousSupport: false }) {
+    super();
+  }
+
   handleRequest(err, user, info) {
     logger.log(`handleRequest ${JSON.stringify({ err, user, info })}`);
     if (err || !user) {
+      if (this.opts.anonymousSupport) {
+        return null;
+      }
       throw err || new UnauthorizedException();
     }
     return user;
