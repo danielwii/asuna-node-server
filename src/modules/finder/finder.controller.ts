48b9cd60502274d64b6536aff7107bdf40be8e4b
Daniel Wei <danielwii0326@gmail.com>
2019-06-15 22:13:24 +0800

    feat: update finder handler

--- src/modules/finder/finder.controller.ts
+++ src/modules/finder/finder.controller.ts
@@ -1,8 +1,11 @@
-import * as _ from 'lodash';
 import { Controller, Get, HttpStatus, Logger, Query, Req, Res } from '@nestjs/common';
 import { Cryptor } from 'node-buffs';
+import { plainToClass } from 'class-transformer';
+import { IsIn, IsOptional, IsString, validate } from 'class-validator';
+import * as _ from 'lodash';
 import * as querystring from 'querystring';
 import { AsunaCollections, KvService } from '../core';
+import urljoin = require('url-join');
 
 const logger = new Logger('FinderController');
 
@@ -10,6 +13,15 @@ const keyByType = {
   zones: 'settings.finder.zones',
   assets: 'settings.finder.assets',
 };
+
+export class FinderAssetsSettings {
+  @IsIn(['https', 'http'])
+  @IsOptional()
+  protocol: 'https' | 'http';
+
+  @IsString()
+  hostname: string;
+}
 
 /**
  * api/v1/finder?query=des-encoded-base64&useEncrypt=true
@@ -32,7 +44,7 @@ export class FinderController {
       !(_.isString(query) && query.length > 0) ||
       !(_.isString(type) && ['zones', 'assets'].includes(type))
     ) {
-      return res.status(HttpStatus.I_AM_A_TEAPOT).end();
+      return res.status(HttpStatus.BAD_REQUEST).end();
     }
 
     const queryParam = querystring.parse(useEncrypt ? Cryptor.desDecrypt(query) : query) as any;
@@ -41,12 +53,29 @@ export class FinderController {
     logger.log(`upstreams ${JSON.stringify(upstreams)}`);
     if (!(upstreams && upstreams.value && _.isObject(upstreams.value))) {
       logger.warn(`${queryParam.name || 'default'} not available in upstream ${keyByType[type]}`);
-      return res.status(HttpStatus.I_AM_A_TEAPOT).end();
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).end();
     }
-    let upstream = upstreams.value[queryParam.name || 'default'];
-    return res.redirect(
-      HttpStatus.TEMPORARY_REDIRECT,
-      `${upstream.protocol || 'https'}://${upstream.hostname}/${queryParam.path}`,
-    );
+
+    if (type === 'assets') {
+      const upstream = upstreams.value[queryParam.name || 'default'];
+      const finderAssetsSettings = plainToClass(FinderAssetsSettings, upstream);
+      if (!finderAssetsSettings) {
+        logger.warn(`invalid upstream ${JSON.stringify(upstream)}`);
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).end();
+      }
+      const errors = await validate(finderAssetsSettings);
+      if (errors.length) {
+        logger.warn(`invalid settings ${JSON.stringify(errors)}`);
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).end();
+      }
+      return res.redirect(
+        HttpStatus.TEMPORARY_REDIRECT,
+        `${upstream.protocol || 'https'}://${urljoin(upstream.hostname, queryParam.path)}`,
+      );
+    } else {
+      // TODO add other handlers later
+      logger.warn('only type assets available');
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).end();
+    }
   }
 }
