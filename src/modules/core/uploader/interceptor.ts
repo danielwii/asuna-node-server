d2abfe91aa0af114b6b5574e2d6fc2c2b808f932
Daniel Wei <danielwii0326@gmail.com>
2019-07-17 14:10:04 +0800

    fix: type issue

--- src/modules/core/uploader/interceptor.ts
+++ src/modules/core/uploader/interceptor.ts
@@ -1,7 +1,7 @@
 import { CallHandler, ExecutionContext, Logger, NestInterceptor } from '@nestjs/common';
 import * as bluebird from 'bluebird';
-import { FastifyRequest } from 'fastify';
-import 'fastify-multipart';
+// import { FastifyRequest } from 'fastify';
+// import 'fastify-multipart';
 import { Observable } from 'rxjs';
 import { fromPromise } from 'rxjs/internal-compatibility';
 import { switchMap } from 'rxjs/operators';
@@ -17,7 +17,7 @@ export type FastifyUploadedFile = {
   mimetype: string;
   encoding: string;
 };
-export type FastifyUploadedFileRequest = FastifyRequest & {
+export type FastifyUploadedFileRequest = /*FastifyRequest &*/ any & {
   file: FastifyUploadedFile;
   files: FastifyUploadedFile[];
 };
@@ -38,10 +38,11 @@ export class FastifyFileInterceptor implements NestInterceptor {
     return fromPromise(
       new Promise((resolve, reject) => {
         const promises = [];
+        /*
         request.multipart(
           (field, file: NodeJS.ReadableStream, filename, encoding, mimetype) => {
             logger.log(
-              `handle file upload ${r({ field, /*file, */ filename, encoding, mimetype })}`,
+              `handle file upload ${r({ field, /!*file, *!/ filename, encoding, mimetype })}`,
             );
             const tempFilename = `${uuid.v4()}.${mimetype.split('/').slice(-1)}__${filename}`;
             const promise = ReqHelper.saveFile(file, tempFilename).then(path => {
@@ -61,6 +62,7 @@ export class FastifyFileInterceptor implements NestInterceptor {
             });
           },
         );
+*/
       }),
     ).pipe(switchMap(() => next.handle()));
   }
