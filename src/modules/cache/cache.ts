18a3bd0a6054b7d6e09bf66d437e9b4d5e21277f
Daniel Wei <danielwii0326@gmail.com>
2019-11-25 23:59:48 +0800

    fix: tests

--- src/modules/cache/cache.ts
+++ src/modules/cache/cache.ts
@@ -1,18 +1,19 @@
+import * as _ from 'lodash';
 import * as LRU from 'lru-cache';
-import _ = require('lodash');
 
 export class CacheManager {
   static cache = new LRU<string, any>({
     max: 500,
     maxAge: 1000 * 60 * 60, // 60 min
   });
+
   static shortCache = new LRU<string, any>({
     max: 50,
     maxAge: 1000 * 60, // 5 min
   });
 
-  static async cacheable(key: string | object, resolver: () => Promise<string>, maxAge?: number) {
-    const cacheKey = _.isString(key) ? key : JSON.stringify(key);
+  static async cacheable(key: string | object, resolver: () => Promise<string>, maxAge?: number): Promise<string> {
+    const cacheKey = _.isString(key) ? (key as string) : JSON.stringify(key);
     const cacheValue = this.cache.get(cacheKey);
     if (cacheValue) return cacheValue;
 
@@ -24,7 +25,7 @@ export class CacheManager {
 
 export function Cacheable(options: { type?: 'default' | 'short'; key: string }) {
   return function(target, propertyKey: string, descriptor: PropertyDescriptor) {
-    let cache = CacheManager.cache;
+    let { cache } = CacheManager;
     if (options.type === 'short') {
       cache = CacheManager.shortCache;
     }
