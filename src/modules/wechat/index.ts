286da27fb7e96c405a6cb362a3fedc084c67f8ae
Daniel Wei <danielwii0326@gmail.com>
2020-01-10 18:30:16 +0800

    refactor: export wechat entities

--- src/modules/wechat/index.ts
+++ src/modules/wechat/index.ts
@@ -1,7 +1,7 @@
 export * from "./interfaces";
 export * from "./wechat.auth";
 // export * from "./wechat.controller";
-// export * from "./wechat.entities";
+export * from "./wechat.entities";
 export * from "./wechat.helper";
 export * from "./wechat.module";
 // export * from "./wx.api";
