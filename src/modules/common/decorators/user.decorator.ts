3f1f5ec047b6f169f152fa8c004174971f3aec6a
Daniel Wei <danielwii0326@gmail.com>
2020-04-06 13:39:57 +0800

    fix: nestjs migration

--- src/modules/common/decorators/user.decorator.ts
+++ src/modules/common/decorators/user.decorator.ts
@@ -1,13 +1,16 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const CurrentUser = createParamDecorator((data, req) => {
-  return req.user;
+export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  return request.user;
 });
 
-export const CurrentTenant = createParamDecorator((data, req) => {
-  return req.tenant;
+export const CurrentTenant = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  return request.tenant;
 });
 
-export const CurrentRoles = createParamDecorator((data, req) => {
-  return req.roles;
+export const CurrentRoles = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  return request.roles;
 });
