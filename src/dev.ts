c5e086ad9a49400a895dbb9cc3e0dd4d3d7308cd
Daniel Wei <danielwii0326@gmail.com>
2020-05-26 20:02:54 +0800

    fix: dev bootstrap

--- src/dev.ts
+++ src/dev.ts
@@ -3,32 +3,12 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {
-  AdminInternalModule,
-  bootstrap,
-  GraphqlModule,
-  LoggerFactory,
-  MQHealthIndicator,
-  RedisHealthIndicator,
-  TerminusOptionsService,
-  WSModule,
-} from '.';
+import { AdminInternalModule, bootstrap, GraphqlModule, LoggerFactory, WSModule } from '.';
 
 const logger = LoggerFactory.getLogger('ApplicationModule');
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    AdminInternalModule,
-    GraphqlModule.forRoot(__dirname),
-    WSModule,
-    TerminusModule.forRootAsync({
-      useClass: TerminusOptionsService.withHealthIndicators(
-        async () => new RedisHealthIndicator().isHealthy('redis'),
-        async () => new MQHealthIndicator().isHealthy('mq'),
-      ),
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(), AdminInternalModule, GraphqlModule.forRoot(__dirname), WSModule, TerminusModule],
   controllers: [],
 })
 export class ApplicationModule implements OnModuleInit {
