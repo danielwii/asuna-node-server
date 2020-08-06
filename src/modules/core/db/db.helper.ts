e106b084b495bb6f11f651b112acffa424b0564b
Daniel Wei <danielwii0326@gmail.com>
2020-08-06 21:05:42 +0800

    fix: relation selection issue

--- src/modules/core/db/db.helper.ts
+++ src/modules/core/db/db.helper.ts
@@ -220,8 +220,8 @@ export class DBHelper {
     opts: { module?: string; prefix?: string },
   ): string {
     let selectable;
-    if ((relation.type as any).entityInfo) {
-      selectable = ((relation.type as any).entityInfo as EntityMetaInfoOptions).name;
+    if (relation.target.entityInfo) {
+      selectable = (relation.target.entityInfo as EntityMetaInfoOptions).name;
     } else {
       const { tableName } = relation.inverseEntityMetadata;
       const name = tableName.slice(`${opts.module}${opts.prefix}`.length);
