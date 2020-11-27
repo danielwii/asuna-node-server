8df01395f6ceb9365823aa741e87fd1ce5694f66
Daniel Wei <danielwii0326@gmail.com>
2020-11-28 02:02:50 +0800

    refactor: entity meta

--- src/modules/client/entities.ts
+++ src/modules/client/entities.ts
@@ -74,6 +74,7 @@ export class VirtualSession extends NoPrimaryKeyBaseEntity {
   public device?: VirtualDevice;
 }
 
+@EntityMetaInfo({ name: 'client__t_session_users', internal: true })
 @Entity('client__t_session_users')
 export class SessionUser extends InjectMultiUserProfile(NoPrimaryKeyBaseEntity) {
   public static generator = new SimpleIdGenerator('imu');
