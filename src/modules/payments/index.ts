47602a408d8848b3870cfd3993549ecc0cc9e6c5
Daniel Wei <danielwii0326@gmail.com>
2021-03-10 00:57:14 +0800

    fix: payment notify exports issue

--- src/modules/payments/index.ts
+++ src/modules/payments/index.ts
@@ -1,4 +1,5 @@
 export * from './payment.controller';
 export * from './payment.entities';
+export * from './payment.notify';
 export * from './payment.order.entities';
 export * from './payment.helper';
